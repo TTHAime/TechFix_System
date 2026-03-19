41949cfb1df4bda0405461aa616308b9151e10a8
Time_thp <thanaphon.thpcd@gmail.com>
2026-03-19 15:52:24 +0700

    add role endpoint

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { RolesModule } from './roles/roles.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
